fdcb42b480f5a14ebd10f4613bb79c837edccd63
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-01-14 18:12:46 -0300

    Tratamento de erros melhorado

--- app/util/exception-utility.js
+++ app/util/exception-utility.js
@@ -1,24 +1,25 @@
 module.exports = {
     internalError(err) {
-        if (!err.messageDev && (!err.status || err.status >= 500))
-            return Object.assign(err, { status: 500, messageDev: err.message, 
-                message: 'Erro Interno! Por Favor, Contatar seu Suporte!' })
+        if (!err.messageClient && (!err.status || err.status >= 500))
+            return Object.assign(err, { status: 500, message: err.message, 
+                messageClient: 'Erro Interno! Por Favor, Contatar seu Suporte!' })
         return err
     },
 
     IlegallArgumentError: class extends Error {
-        constructor (message, messageDev, status=400) {
+        constructor (messageClient, message, status=400) {
             super(message)
-            if (!messageDev) messageDev = message
-            this.messageDev = messageDev
+            if (!messageClient) messageClient = message
+            this.messageClient = messageClient
             this.status = status
         }
     },
 
     RuntimeError: class extends Error {
-        constructor (message, messageDev, status=500) {
+        constructor (messageClient, message, status=500) {
             super(message)
-            this.messageDev = messageDev
+            if (!messageClient) messageClient = message
+            this.messageClient = messageClient
             this.status = status
         }
     }
