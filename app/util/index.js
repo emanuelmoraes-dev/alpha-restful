d5cae27fc4530796d36fb38548f8eecc379d5261
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-03-17 13:04:18 -0300

    Chamada de js-adapter em util/index.js removida

--- app/util/index.js
+++ app/util/index.js
@@ -2,6 +2,5 @@ module.exports = {
 	dateUtility: require('./date-utility'),
 	dbUtility: require('./db-utility'),
 	exceptionUtility: require('./exception-utility'),
-	jsAdapter: require('./js-adapter'),
 	utility: require('./utility')
 }
