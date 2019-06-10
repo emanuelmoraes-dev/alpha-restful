840090159c43458ed9b83dae3b604ca922df6eeb
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-06-10 10:45:39 -0300

    date-utility removed

--- app/util/index.js
+++ app/util/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-	dateUtility: require('./date-utility'),
 	dbUtility: require('./db-utility'),
 	exceptionUtility: require('./exception-utility'),
 	utility: require('./utility')
