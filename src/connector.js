4789658b19e188e2ecf39bc60bc8b53595d240b6
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-08-01 01:26:25 -0300

    connector comentado e com tratamento de erro quando nao é informado a conexão com o MongoDB

--- src/connector.js
+++ src/connector.js
@@ -1,37 +1,45 @@
 const mongoose = require('mongoose')
+const { InternalError } = require('./errors/generic_errors')
 const Restful = require('./restful')
 const db = mongoose.connection
 
-module.exports = class Connector {
-	constructor (dbName, host, restful, app, useNewUrlParser) {
-		if (host instanceof Restful) {
-			this.url = dbName
-			this.useNewUrlParser = app
-			this.app = restful
-			this.restful = host
-			this.host = null
-			this.dbName = null
-		} else {
-			this.url = null
-			this.dbName = dbName
-			this.host = host
-			this.restful = restful
-			this.app = app
-			this.useNewUrlParser = useNewUrlParser
-		}
+/**
+ * Responsável por encapsular a instância do Alpha Restful e 
+ * fornecer métodos para conectar o Alpha Restful com o MongoDB.
+ */
+class Connector {
+	/**
+	 * Responsible for encapsulating the Alpha Restful instance and 
+	 * providing methods for connecting Alpha Restful with MongoDB.
+	 * @param {object} restful - Alpha Restful Instance
+	 * @param {object} app - Express JS instance
+	 * @param {object} connInfo
+	 * @param {boolean} [connInfo.useNewUrlParser] - Flag used for mongoose. See on {@link https://mongoosejs.com/docs/deprecations.html#the-usenewurlparser-option}
+	 * @param {string} [connInfo.url] - String used to connect to MongoDB
+	 * @param {string} [connInfo.host] - Database location. Example: 'localhost'
+	 * @param {string} [connInfo.dbName] - Database Name
+	 */
+	constructor (restful, app, { useNewUrlParser=true, url=null, host=null, dbName=null }={}) {
+		if (!url && (!host || !dbName)) 
+			throw new InternalError("The 'url' parameter is required unless the 'host' and 'dbName' parameters are passed")
 
-		if (this.useNewUrlParser === undefined)
-			this.useNewUrlParser = true
-
-		this.useNewUrlParser = !!this.useNewUrlParser
+		if (!url) 
+			url = `mongodb://${host}/${dbName}`
+		
+		this.url = url
+		this.restful = restful
+		this.app = app
+		this.useNewUrlParser = useNewUrlParser
 	}
 
-	onError(reject, err) {
+	// Executed if an error occurs while connecting to MongoDB
+	_onError(reject, err) {
 		reject(err)
 		console.error('connection error:', err)
 	}
 
-	onOpen(resolve) {
+	// Executed if MongoDB connection succeeds
+	_onOpen(resolve) {
 		this.restful.debug('applying mongodb modeling ...')
 		this.restful.structDb()
 		this.restful.debug('modeling applied to mongodb')
@@ -40,19 +48,18 @@ module.exports = class Connector {
 		this.restful.debug('established connection with mongodb')
 	}
 
+	/**
+	 * Connect Alpha Restful to MongoDB
+	 * @returns {Promise}
+	 */
 	async connect () {
-		if (this.url) {
-			this.restful.debug(`establishing connection to ${this.url} ...`)
-			mongoose.connect(this.url, { useNewUrlParser: this.useNewUrlParser })
-		}
-		else {
-			this.restful.debug(`establishing connection to mongodb://${this.host}/${this.dbName} ...`)
-			mongoose.connect(`mongodb://${this.host}/${this.dbName}`, { useNewUrlParser: this.useNewUrlParser })
-		}
-
+		this.restful.debug(`establishing connection to ${this.url} ...`)
+		mongoose.connect(this.url, { useNewUrlParser: this.useNewUrlParser })
 		return await (new this.restful.Promise((resolve, reject) => {
-			db.on('error', this.onError.bind(this, reject))
-			db.once('open', this.onOpen.bind(this, resolve))
+			db.on('error', this._onError.bind(this, reject))
+			db.once('open', this._onOpen.bind(this, resolve))
 		}))
 	}
 }
+
+module.exports = Connector
